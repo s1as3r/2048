ec4c39667f337464467f3e5116298120aa730f3d
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2024-12-21 16:32:50 +0530

    fix move over empty cells logic

--- src/cell_moves.lua
+++ src/cell_moves.lua
@@ -2,17 +2,38 @@ local util = require("util")
 
 local cell_moves = {}
 
+local function moveOverEmptyCellsInRow(cells, row, from, to, direction)
+    for j = from, to, direction do
+        if cells[row][j] ~= 0 then
+            local i = j - direction;
+            while i > 0 and i <= #cells[row] and cells[row][i] == 0 do
+                cells[row][i] = cells[row][i + direction]
+                cells[row][i + direction] = 0
+                i = i - direction
+            end
+        end
+    end
+end
+
+local function moveOverEmptyCellsInCol(cells, col, from, to, direction)
+    for j = from, to, direction do
+        if cells[j][col] ~= 0 then
+            local i = j - direction
+            while i > 0 and i <= #cells and cells[i][col] == 0 do
+                cells[i][col] = cells[i + direction][col]
+                cells[i + direction][col] = 0
+                i = i - direction
+            end
+        end
+    end
+end
+
 function cell_moves.moveRight(cells)
     local score = 0;
 
-    for _, row in ipairs(cells) do
+    for i, row in ipairs(cells) do
         local toMerge = {}
-        for j = 1, #row - 1 do
-            if row[j] ~= 0 and row[j + 1] == 0 then
-                row[j + 1] = row[j]
-                row[j] = 0
-            end
-        end
+        moveOverEmptyCellsInRow(cells, i, #row - 1, 1, -1)
 
         for j = 1, #row - 1 do
             if row[j] ~= 0 and row[j] == row[j + 1] then
@@ -28,12 +49,7 @@ function cell_moves.moveRight(cells)
             end
         end
 
-        for j = 1, #row - 1 do
-            if row[j] ~= 0 and row[j + 1] == 0 then
-                row[j + 1] = row[j]
-                row[j] = 0
-            end
-        end
+        moveOverEmptyCellsInRow(cells, i, #row - 1, 1, -1)
     end
 
     return score
@@ -42,14 +58,9 @@ end
 function cell_moves.moveLeft(cells)
     local score = 0;
 
-    for _, row in ipairs(cells) do
+    for i, row in ipairs(cells) do
         local toMerge = {}
-        for j = #row, 2, -1 do
-            if row[j] ~= 0 and row[j - 1] == 0 then
-                row[j - 1] = row[j]
-                row[j] = 0
-            end
-        end
+        moveOverEmptyCellsInRow(cells, i, 2, #row, 1)
 
         for j = #row, 2, -1 do
             if row[j] ~= 0 and row[j] == row[j - 1] then
@@ -65,12 +76,7 @@ function cell_moves.moveLeft(cells)
             end
         end
 
-        for j = #row, 2, -1 do
-            if row[j] ~= 0 and row[j - 1] == 0 then
-                row[j - 1] = row[j]
-                row[j] = 0
-            end
-        end
+        moveOverEmptyCellsInRow(cells, i, 2, #row, 1)
     end
 
     return score
@@ -81,13 +87,7 @@ function cell_moves.moveUp(cells)
 
     for i = 1, #cells[1] do
         local toMerge = {}
-
-        for j = #cells, 2, -1 do
-            if cells[j][i] ~= 0 and cells[j - 1][i] == 0 then
-                cells[j - 1][i] = cells[j][i]
-                cells[j][i] = 0
-            end
-        end
+        moveOverEmptyCellsInCol(cells, i, 2, #cells, 1)
 
         for j = #cells, 2, -1 do
             if cells[j][i] ~= 0 and cells[j][i] == cells[j - 1][i] then
@@ -103,12 +103,7 @@ function cell_moves.moveUp(cells)
             end
         end
 
-        for j = #cells, 2, -1 do
-            if cells[j][i] ~= 0 and cells[j - 1][i] == 0 then
-                cells[j - 1][i] = cells[j][i]
-                cells[j][i] = 0
-            end
-        end
+        moveOverEmptyCellsInCol(cells, i, 2, #cells, 1)
     end
 
     return score
@@ -119,13 +114,7 @@ function cell_moves.moveDown(cells)
 
     for i = 1, #cells[1] do
         local toMerge = {}
-
-        for j = 1, #cells - 1 do
-            if cells[j][i] ~= 0 and cells[j + 1][i] == 0 then
-                cells[j + 1][i] = cells[j][i]
-                cells[j][i] = 0
-            end
-        end
+        moveOverEmptyCellsInCol(cells, i, #cells - 1, 1, -1)
 
         for j = 1, #cells - 1 do
             if cells[j][i] ~= 0 and cells[j][i] == cells[j + 1][i] then
@@ -141,12 +130,7 @@ function cell_moves.moveDown(cells)
             end
         end
 
-        for j = 1, #cells - 1 do
-            if cells[j][i] ~= 0 and cells[j + 1][i] == 0 then
-                cells[j + 1][i] = cells[j][i]
-                cells[j][i] = 0
-            end
-        end
+        moveOverEmptyCellsInCol(cells, i, #cells - 1, 1, -1)
     end
 
     return score
